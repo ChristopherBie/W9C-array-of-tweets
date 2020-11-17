2e800aca43580d16320a8b61f895867130afb543
Chris <newuser@News-MBP.hitronhub.home>
2020-11-17 16:08:18 -0700

    array of tweets

---
+++ js/app.js
@@ -0,0 +1,24 @@
+var tweets = [
+'Operation Warp Speed is unequaled and unrivaled anywhere in the world.',
+'This is 5 times faster than the fastest prior vaccine development in history.',
+'We cannot waste time and can only give to those states that will use the Vaccine immediately.',
+'Therefore the New York delay. Many lives to be saved, but we are ready when they are. Stop playing politics!',
+'Congress must now do a Covid Relief Bill. Needs Democrats support. Make it big and focused. Get it done!',
+'Help save lives by donating blood or plasma, joining Canada’s stem cell registry, and encouraging others to donate',
+'Albertans, we need your help! All blood types are needed. Please encourage a family member, friend, or colleague to donate blood as well.',
+'Jason Kenney: These major upgrades will help boost the quality of patient care offered at the PL hospital, while creating hundreds of jobs when we need them most.',
+'Memory Anchor is creating a new free mobile app so citizens can share stories of soldiers buried in war graves, starting Spring 2021.',
+'A good read from Dr. Leslyn Lewis: A strong society must encourage, not limit, debate.',
+];
+
+for(i = 0; i < tweets.length; i++) {
+    console.log(tweets[i]);
+}
+
+
+
+
+
+
+
+
